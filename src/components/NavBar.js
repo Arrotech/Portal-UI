c63ad367c5024952a361a74c10125d32557af602
Harun Gachanja <arrotechdesign@gmail.com>
2020-12-04 17:28:20 +0300

    Feature (Student Login UI)
    What does this pr do?
    
    Design and develop student login user interface.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../assets/css/NavBar.css'
 import SchoolIcon from '@material-ui/icons/School';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 function NavBar() {
     return (
         <div className="navBar">
             <div className="navBar__left">
-                <SchoolIcon className="navBar__schoolIcon"/>
+                <Link className="navBar__rightLinks" to="/"><SchoolIcon className="navBar__schoolIcon" /></Link>
             </div>
             <div className="navBar__right">
                 <Link className="navBar__rightLinks" to="/staff"><p>Staff</p></Link>
--- src/pages/StudentLogin.js
+++ src/pages/StudentLogin.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import '../assets/css/StudentLogin.css'
+import { Link } from 'react-router-dom'
 
 function StudentLogin() {
     return (
         <div className="studentLogin">
-            <h1>Student Login</h1>
+            <div className="studentLogin__container">
+                <div className="studentLogin__left">
+                    <div className="studentLogin__topLeft">Student Portal</div>
+                    <div className="studentLogin__centered">
+                        <p>There is always a room to increase your knowledge.</p>
+                        <p>Secure you future with a course.</p>
+                    </div>
+                    <div className="studentLogin__centeredParagraph">
+                        @ 2019 ARROTECH DESINGs
+                    </div>
+                </div>
+                <div className="studentLogin__right">
+                    <h1>Welcome back,</h1>
+                    <h2>Login to your account</h2>
+                    <input type="text" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <p className="studentLogin__radioBtn"><input type="checkbox" /><span className="studentLogin__checkBoxText">Remember me</span></p>
+                    <input className="studentLogin__btn" type="submit" value="Login" />
+                    <p>Forget Password? <Link className="navBar__rightLinks" to="/reset"><span>Reset</span></Link></p>
+                </div>
+            </div>
         </div>
     )
 }
